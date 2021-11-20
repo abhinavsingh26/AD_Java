1e721225d776c13d957eaa3b17b1e15de25f2922
Abhinav Singh <abhinavsingh26@gmail.com>
2021-11-20 06:51:36 +0530

    Pangram updated

--- Borrowd/src/Pangram.java
+++ Borrowd/src/Pangram.java
@@ -1,26 +1,38 @@
 import java.util.Scanner;
 
-class Pangram
+public class Pangram
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
+       // String str = " The quick brown fox jumps over the lazy dog";
+       // str = str.toLowerCase();
         
-        str = str.toLowerCase();
+        boolean[] alphaList = new boolean[100];
+        int index  = 0;
+        int flag = 1;
         
-        boolean allLetterPresent = true;
- 
-        // Loop over each character itself
-        for (char ch = 'a'; ch <= 'z'; ch++) 
+        for (int i = 0; i < str.length(); i++ )
         {
-            if (!str.contains(String.valueOf(ch))) 
-            {
-                allLetterPresent = false;
-                break;
-            }
+        	if (str.charAt(i) >= 'A' && str.charAt(i)  <= 'Z')
+        	{
+        		index = str.charAt(i) - 'A';
+        	}
+        	else if( str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+        	{
+        		index = str.charAt(i) - 'a';
+        	}
+        	alphaList[index] = true;
         }
-        if (allLetterPresent)
+        
+        for (int i = 0; i  <= 25; i++) 
+        {
+            if (alphaList[i] == false) 
+            	flag = 0;
+        }
+     
+        if (flag == 1)
         {
             System.out.println("fully loaded!");
         }
